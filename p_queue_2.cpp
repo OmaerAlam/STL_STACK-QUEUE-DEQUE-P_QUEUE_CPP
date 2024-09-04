b797c340bafd60a25e72f31f51f1f4e089cc88f3
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2024-09-04 11:47:55 +0600

    Add files via upload

---
+++ p_queue_2.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    priority_queue<int> q;
+
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(5);
+
+    cout<<q.size()<<endl;
+    while(!q.empty()){
+        cout<<q.top()<<" ";
+        q.pop();
+    }
+
+
+return 0;
+}
+
+
+
