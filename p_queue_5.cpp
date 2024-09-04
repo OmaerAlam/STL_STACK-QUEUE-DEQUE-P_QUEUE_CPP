bda0c61cf238dcc357112d7ef69f8f1c7cde38d0
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2024-09-04 11:49:03 +0600

    Add files via upload

---
+++ p_queue_5.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    priority_queue<pair<int,int>> q;
+    q.push({1,2});
+    q.push({3,4});
+    q.push({7,1});
+    q.push({2,5});
+    q.push({5,1});
+
+    cout<<q.size()<<endl;
+    while(!q.empty()){
+        cout<<q.top().first<<" "<<q.top().second<<endl;;
+        q.pop();
+    }
+
+return 0;
+}
+
+
+
+
+
+
